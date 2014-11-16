80b27e5a8a31ead6bdd232b3025847f77b0e7778
mgonzalez1 <moisesgonzalez2001@hotmail.com>
2014-11-16 11:17:15 -0430

    Sudoku arreglado y terminado

--- sudoku.js
+++ sudoku.js
@@ -2,7 +2,7 @@ function celdaDisponible(grid,x,y){
 	var i;
 	var j;
 	for(i=x; i<grid.length; i++){
-		for(j=y; j<grid.length; j++){
+		for(j=0; j<grid.length; j++){
 			if(grid[i][j]==0){
 				return [i,j];
 			}
@@ -24,7 +24,6 @@ function hackPrintMatriz(maat){
 	return ress;
 }
 
-
 function noConflicto(tablero,x,y,valor){
 	//INDICES PARA MATRIZ DE SECTOR
 	var xx=Math.floor(x/3);//saca los indices de los for para la el sector
@@ -41,6 +40,7 @@ function noConflicto(tablero,x,y,valor){
 			return false;
 
 	}
+	
 	var lxx=xx+3;
 	var lyy=yy+3;
 	for (xx;xx<lxx;xx++){
@@ -51,6 +51,26 @@ function noConflicto(tablero,x,y,valor){
 	}
 	return true;
 
+}
+
+function main(grid, x, y) {
+    var disp=celdaDisponible(grid,x,y);
+    var x=disp[0];
+    var y=disp[1];
+    if (x==-1 || y==-1) {
+        //Listo
+        return true;
+    }
+    for(var i=1; i<=9; i++){
+        if (noConflicto(grid,x,y,i)) {
+            grid[x][y]=i;
+            if (main(grid,x,y))
+                return true;
+        }
+        grid[x][y]=0;
+    }
+        return false;
+        // TERMINAR !!!
 }
 
 var grid=[
@@ -64,38 +84,3 @@ var grid=[
 	[0,0,0,5,6,0,8,0,2],
 	[4,0,0,0,0,0,0,0,0]
 	];
-/*function main(){
-	var tablero=[
-	[0,0,8,1,0,2,3,0,0],
-	[0,0,3,0,8,7,5,0,0],
-	[0,0,4,0,0,0,0,0,0],
-	[0,0,0,2,0,0,0,0,0],
-	[0,0,7,0,0,5,0,0,6],
-	[2,0,0,7,0,6,9,4,0],
-	[6,0,2,8,0,3,0,0,0],
-	[0,0,0,5,6,0,8,0,2],
-	[4,0,0,0,0,0,0,0,0]
-	];
-	console.log(hackPrintMatriz(tablero));
-}*/
-
-function main(grid, x, y) {
-	var disp=celdaDisponible(grid,x,y);
-	var x=disp[0];
-	var y=disp[1];
-	if (x==-1 && y==-1) {
-		//Listo
-		return true;
-	}
-	for(var i=1; i<=9; i++){
-		if (noConflicto(grid,x,y,i)) {
-			grid[x][y]=i;
-			if (main(grid,x,y)) {
-				return true;
-			}
-		}
-		grid[x][y]=0;
-	}
-		return false;
-		// TERMINAR !!!
-}
